a0f75371c5c01e4c948c4cfd6531368db0657884
John Roller <jtroller33@gmail.com>
2023-11-05 19:20:13 -0600

    response issue fixed.

--- github_bot.py
+++ github_bot.py
@@ -12,12 +12,9 @@ run_id = random.choice(range(1,100000))     # used to identify a particular run 
 
 response = requests.get(github_url)
 response_list = response.text.splitlines()
-count = ((response_list[-3]).strip())[23:-7]
+count = ((response_list[-3]).strip())[21:-7]    # takes the response object and strips everything except the profile count
 
-max_loop = int(input(f"\nCurrent profile views: {count}.\nHow many times do you want to run the GH bot? "))
-
-
-
+max_loop = int(input(f"\nCurrent profile views: {count}\nHow many times do you want to run the GH bot? "))
 
 for run in range(max_loop):      
     try:
@@ -33,7 +30,7 @@ for run in range(max_loop):
 
         if response:    # response returns boolean when it's in a conditional statement
             response_list = response.text.splitlines()
-            count = ((response_list[-3]).strip())[23:-7]        # takes the response object and strips everything except the profile count
+            count = ((response_list[-3]).strip())[21:-7]    # takes the response object and strips everything except the profile count
             print(f"[*] Response received! New profile view count: {count}")
 
         response.raise_for_status()     # raises an Exception if an error occured
